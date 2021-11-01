a1271d73dbe2658e95c43092e7946884e13b5528
acomanzo <ansonih17@gmail.com>
2021-11-01 00:19:45 -0400

    add apollo client to project

--- frontend/index.js
+++ frontend/index.js
@@ -9,16 +9,25 @@ import App from './App';
 import { name as appName } from './app.json';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { NavigationContainer } from '@react-navigation/native';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+    uri: 'localhost:8989/graphql', 
+    cache: new InMemoryCache(),
+});
 
 export default function Main() {
+
     return (
-        <GestureHandlerRootView style={{flex: 1}}>
-            <NavigationContainer>
-                <PaperProvider>
-                    <App />
-                </PaperProvider>
-            </NavigationContainer>
-        </GestureHandlerRootView> 
+        <ApolloProvider client={client}>
+            <GestureHandlerRootView style={{flex: 1}}>
+                <NavigationContainer>
+                    <PaperProvider>
+                        <App />
+                    </PaperProvider>
+                </NavigationContainer>
+            </GestureHandlerRootView> 
+        </ApolloProvider>
     );
 }
 
