20235bde5d471364ffad989e846774198a0c3fe6
acomanzo <ansonih17@gmail.com>
2021-11-11 19:27:35 -0500

    refactor by creating component that wraps the DateTimePickerModal library and removing relevant logic from CreateLog and putting it into that wrapper

---
+++ frontend/src/components/atoms/date_time_picker/DateTimePicker.tsx
@@ -0,0 +1,79 @@
+import React, { useState, useContext } from 'react';
+import DateTimePickerModal from 'react-native-modal-datetime-picker';
+
+export enum dateTimePickerModes {
+    Date = "date", 
+    Time = "time",
+    DateTime = "datetime",
+}
+
+type DateTimePickerProps = {
+    selectedDate: Date;
+    setSelectedDate: Function;
+    selectedTime: Date;
+    setSelectedTime: Function;
+    selectedDateTime: number;
+    setSelectedDateTime: Function;
+    isDatePickerVisible: boolean;
+    setIsDatePickerVisible: Function;
+    dateTimePickerMode: dateTimePickerModes;
+    setDateTimePickerMode: Function;
+
+};
+
+const DateTimePicker = (props: DateTimePickerProps) => {
+
+    const handleDateConfirmed = (date: Date) => {
+        switch (props.dateTimePickerMode) {
+            case dateTimePickerModes.Date: 
+                {
+                    const dateTime = new Date(
+                        date.getFullYear(),
+                        date.getMonth(), 
+                        date.getDate(), 
+                        props.selectedTime.getHours(), 
+                        props.selectedTime.getMinutes(), 
+                        props.selectedTime.getSeconds(), 
+                        props.selectedTime.getMilliseconds()
+                    );
+                    const epoch = dateTime.getTime() / 1000;
+                    const epochDate = new Date(0);
+                    epochDate.setUTCSeconds(epoch);
+                    props.setSelectedDateTime(epoch);
+                    props.setSelectedDate(date);
+                    props.setIsDatePickerVisible(false);
+                }
+                break;
+            case dateTimePickerModes.Time: 
+                {
+                    const dateTime = new Date(
+                        props.selectedDate.getFullYear(),
+                        props.selectedDate.getMonth(), 
+                        props.selectedDate.getDate(), 
+                        date.getHours(), 
+                        date.getMinutes(), 
+                        date.getSeconds(), 
+                        date.getMilliseconds()
+                    );
+                    const epoch = dateTime.getTime() / 1000;
+                    const epochDate = new Date(0);
+                    epochDate.setUTCSeconds(epoch);
+                    props.setSelectedDateTime(epoch);
+                    props.setSelectedTime(date);
+                    props.setIsDatePickerVisible(false);
+                }
+                break;
+        }
+    };
+
+    return (
+        <DateTimePickerModal 
+            isVisible={props.isDatePickerVisible}
+            mode={props.dateTimePickerMode}
+            onConfirm={handleDateConfirmed}
+            onCancel={() => props.setIsDatePickerVisible(false)}
+        />
+    );
+};
+
+export default DateTimePicker;
