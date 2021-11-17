3755bff3d24377e3bb47c0e537683908b1042f64
acomanzo <ansonih17@gmail.com>
2021-11-17 03:57:05 -0500

    fix label and tick styling on graphs in MetricDetail, added logic to check for non-numerical values and show bar plot if so

--- frontend/src/screens/MetricDetail.tsx
+++ frontend/src/screens/MetricDetail.tsx
@@ -1,7 +1,7 @@
 import React, { useLayoutEffect } from 'react';
 import { useQuery, useMutation } from '@apollo/client';
-import { Alert, Text, View, StyleSheet } from 'react-native';
-import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis } from 'victory-native';
+import { Alert, Text, View, StyleSheet, ScrollView } from 'react-native';
+import { VictoryLine, VictoryChart, VictoryTheme, VictoryAxis, VictoryLabel, VictoryBar } from 'victory-native';
 import { IconButton } from 'react-native-paper';
 import { AppStackParamList } from '../navigation/AppStack';
 import { NativeStackScreenProps } from '@react-navigation/native-stack';
@@ -88,25 +88,102 @@ const MetricDetail = ({ route, navigation }: MetricDetailProps) => {
 
         const { title, xUnits, yUnits } = data.GetMetricById;
 
-        const sortedData = data.GetMetricById.data.map(item => item).sort((a, b) => a.dateTimeMeasured - b.dateTimeMeasured);
+        // regex to test if strings contains only digits
+        const regex = /^\d+$/;
+        let numbersOnly = true;
+        for (let i = 0; i < data.GetMetricById.data.length; i++) {
+            if (!regex.test(data.GetMetricById.data[i].y.trim())) {
+                numbersOnly = false;
+                break;
+            }
+        }
 
-        return (
-            <View style={styles.container}>
-                <Text style={styles.titleText}>{title}</Text>
-                <VictoryChart 
-                    theme={VictoryTheme.material}
-                >
-                    <VictoryLine data={sortedData} />
-                    <VictoryAxis 
-                        dependentAxis
-                        label={yUnits}
-                    />
-                    <VictoryAxis 
-                        label={xUnits}
-                    />
-                </VictoryChart>
-            </View>
-        );
+        const processedData = data.GetMetricById.data.map(item => {
+            const i = JSON.parse(JSON.stringify(item));
+            if (numbersOnly) {
+                i.y = parseFloat(i.y);
+                return i;
+            } else {
+                return i;
+            }
+        }).sort((a, b) => a.dateTimeMeasured - b.dateTimeMeasured);
+
+        if (numbersOnly) {
+            return (
+                <View style={styles.container}>
+                    <Text style={styles.titleText}>{title}</Text>
+                    <VictoryChart 
+                        width={350}
+                        height={350}
+                        padding={{top: 20, right: 30, bottom: 50, left: 80}}
+                        theme={VictoryTheme.material}
+                    >
+                        <VictoryLine data={processedData} />
+                        <VictoryAxis 
+                            dependentAxis
+                            label={yUnits}
+                            axisLabelComponent={
+                                <VictoryLabel
+                                    dy={-40}
+                                    angle={-90}
+                                />
+                            }
+                        />
+                        <VictoryAxis 
+                            label={xUnits}
+                            axisLabelComponent={
+                                <VictoryLabel
+                                    dy={20}
+                                />
+                            }
+                            tickFormat={(x) => {
+                                const date = new Date(0);
+                                date.setUTCSeconds(x);
+                                const formattedDate = `${date.getMonth() + 1}/${date.getDate()} ${date.getHours()}:${date.getMinutes() < 10 ? `0${date.getMinutes()}` : date.getMinutes()}`;
+                                return formattedDate;
+                                // return date.toLocaleDateString();
+                            }}
+                            fixLabelOverlap
+                        />
+                    </VictoryChart>
+                </View>
+            );
+        } else {
+            let map = new Map();
+            for (let i = 0; i < processedData.length; i++) {
+                const item = processedData[i];
+                const key = item.y.trim().toLowerCase();
+                if (map.has(key)) {
+                    map.set(key, map.get(key) + 1);
+                } else {
+                    map.set(key, 1);
+                }
+            }
+            let data = [];
+            for (const [key, value] of map.entries()) {
+                data.push({ x: key, y: value });
+            }
+
+            return (
+                <View>
+                    <VictoryChart theme={VictoryTheme.material} domainPadding={20}>
+                        <VictoryBar 
+                            data={data}
+                        />
+                        <VictoryAxis
+                            dependentAxis
+                        />
+                        <VictoryAxis
+                            tickLabelComponent={
+                                <VictoryLabel
+                                    angle={-45}
+                                />
+                            }
+                        />
+                    </VictoryChart>
+                </View>
+            );
+        }
     }
 };
 
