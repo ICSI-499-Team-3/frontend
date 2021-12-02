def733934b897bfffd61103932627fb7688fc83e
acomanzo <acomanzo24@gmail.com>
2021-12-01 23:28:58 -0500

    add query and type for GetMetricsBySharerAndShareeId

---
+++ frontend/src/graphql/queries/GetMetricsBySharerAndShareeId.ts
@@ -0,0 +1,21 @@
+import { gql } from '@apollo/client';
+
+const GET_METRICS_BY_SHARER_AND_SHAREE_ID = gql`
+    query GetMetricsBySharerAndShareeId($sharerId: String!, $shareeId: String!) {
+        GetMetricsBySharerAndShareeId(sharerId: $sharerId, shareeId: $shareeId) {
+            id
+            userId
+            title
+            xUnits
+            yUnits 
+            data {
+                id
+                x
+                y 
+                dateTimeMeasured
+            }
+        }
+    }
+`;
+
+export default GET_METRICS_BY_SHARER_AND_SHAREE_ID;
---
+++ frontend/src/graphql/types/GetMetricsBySharerAndShareeIdData.ts
@@ -0,0 +1,7 @@
+import Metric from './Metric';
+
+interface GetMetricsBySharerAndShareeIdData {
+    GetMetricsBySharerAndShareeId: Metric[];
+}
+
+export default GetMetricsBySharerAndShareeIdData;
