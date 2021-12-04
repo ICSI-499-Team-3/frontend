8a8902e1f1c2992d1cddc4c1e596cd9ef9bc67f7
emmawirth <emmawirth4@gmail.com>
2021-12-04 12:11:59 -0500

    changes for card icons to function

--- frontend/src/components/atoms/card_icons/CardIcons.tsx
+++ frontend/src/components/atoms/card_icons/CardIcons.tsx
@@ -1,16 +1,43 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { View, StyleSheet } from 'react-native';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type CardIconsProps = {
-    categories: string[];
+    categories: string[]
 };
 
+//{categories.slice(0, 6).map(category  => <Icon key={category} name={activitiesToIcons.get('running')} size={20} />)}
+//{categories.slice(0, 6).map(category => <Icon key={category} name={activitiesToIcons.get('yoga')} size={20} />)}
+//{categories.slice(0, 6).map(category => <Icon key={category} name={activitiesToIcons.get('eating')} size={20} />)}
+
 const CardIcons = ({ categories }: CardIconsProps) => {
+
+    //categories = ['running', 'yoga', 'going out', 'physical therapy', 'therapy', 'eating', 'spending time with friends'];
+
+    const activitiesToIcons = new Map()
+    activitiesToIcons.set('running', 'run')
+    activitiesToIcons.set('yoga', 'yoga')
+    activitiesToIcons.set('going out', 'party-popper')
+    activitiesToIcons.set('physical therapy', 'doctor')
+    activitiesToIcons.set('therapy', 'account-heart')
+    activitiesToIcons.set('eating', 'food')
+    activitiesToIcons.set('spending time with friends', 'human-male-female')
+
+
+
+  /* {categories.map(category => (
+                        <Icon 
+                        key={category} 
+                        name={activitiesToIcons.get(categories.indexOf('yoga'))} 
+                        size={20}
+                        onPress={() => handlePickCategory(category)}
+                         />
+                    ))}*/
+
     return (
         <View style={[styles.categoriesContainer]}>
-            {categories.slice(0, 3).map(category => <Icon key={category} name={'run'} size={20} />)}
+            {categories.slice(0, 3).map(category => <Icon key={category} name={activitiesToIcons.get(category)} size={20} />)}
         </View>
     );
 };
---
+++ frontend/src/queries/GetUserByEmail 2.ts
@@ -0,0 +1,11 @@
+import { gql } from '@apollo/client';
+
+const GET_USER_BY_EMAIL = gql`
+  query GetUserByEmail($email: String!) { 
+    GetUserByEmail(email: $email) {
+      email
+    }
+  }
+`;
+
+export default GET_USER_BY_EMAIL;
