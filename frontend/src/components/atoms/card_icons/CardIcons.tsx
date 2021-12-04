4dc9644647d5383c1107295b929ea1d3517557d5
theonewhocodes4u <jonathandjules@gmail.com>
2021-12-04 11:22:56 -0500

    Completed cardicons

--- frontend/src/components/atoms/card_icons/CardIcons.tsx
+++ frontend/src/components/atoms/card_icons/CardIcons.tsx
@@ -1,53 +1,27 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { View, StyleSheet } from 'react-native';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
 
 type CardIconsProps = {
     categories: string[];
 };
 
-const CardIcons = ({ categories }: CardIconsProps) => {
-
-    categories = ['running', 'yoga', 'going out', 'physical therapy', 'therapy', 'eating', 'spending time with friends'];
-
-    const [selectedCategories, setSelectedCategories] = useState(new Set<string>());
+ const CardIcons = ({ categories }: CardIconsProps) => {
 
     const activitiesToIcons = new Map()
-    activitiesToIcons.set(categories.indexOf('running'), 'run') // 0
-    activitiesToIcons.set(categories.indexOf('eating'), 'food') // 5
-
-    // console.log(activitiesToIcons);
-
-    const pickedCategory = (category: string) => {
-        if (selectedCategories.has(category)) { // true
-            
-            selectedCategories.delete(category);
-        } else {
-            selectedCategories.add(category);
-        }
-            setSelectedCategories(selectedCategories);
-    };
-
-    // console.log(selectedCategories);
+    activitiesToIcons.set('running', 'run') 
+    activitiesToIcons.set('eating', 'food') 
+    activitiesToIcons.set('going out', 'party-popper') 
+    activitiesToIcons.set('spending time with friends', 'human-male-female')
+    activitiesToIcons.set('physical therapy', 'doctor')
+    activitiesToIcons.set('therapy', 'account-heart')
 
     return (
-        <View style={[styles.categoriesContainer]}>
-            {categories.map(category => (
-            <Icon key={category} name={activitiesToIcons.get(pickedCategory(category))} size={20} />
-        ))}
-        </View> 
+    <View style={[styles.categoriesContainer]}>
+        {categories.slice(0, 3).map(category => <Icon key={category} name={activitiesToIcons.get(category)} size={20} />)}
+    </View>
     );
 };
-
-/*         <View style={[styles.categoriesContainer]}>
-            {categories.slice(0, 3).map(category => <Icon key={category} name={'run'} size={20} />)}
-        </View>
-
-                {<Icon name={activitiesToIcons.get(categories.indexOf('running'))} size={20} />}
-        {<Icon key={categories.indexOf('yoga')} name={activitiesToIcons.get(categories.indexOf('yoga'))} size={20} />}
-
-        */
 
 const styles = StyleSheet.create({
     categoriesContainer: {
