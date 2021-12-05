07d5c58bf30d7bdb6bde0b953cd6765586ffab9b
Habib Affinnih <48428026+haffinnih@users.noreply.github.com>
2021-12-05 14:44:16 -0500

    Implement sharing logs to social media

--- frontend/src/screens/LogDetail.tsx
+++ frontend/src/screens/LogDetail.tsx
@@ -1,20 +1,19 @@
-import React, { useMemo, useRef, useCallback, useLayoutEffect } from 'react';
-import { View, StyleSheet, ScrollView, Button } from 'react-native';
-import { AppStackParamList } from '../navigation/AppStack';
-import { NativeStackScreenProps } from '@react-navigation/native-stack';
-import { Chip, Text } from 'react-native-paper';
-import { IconButton, Colors } from 'react-native-paper';
 import { BottomSheetModal, BottomSheetModalProvider } from '@gorhom/bottom-sheet';
-import LogDetailBottomSheet from '../components/atoms/log_detail/LogDetailBottomSheet';
-import CardIcons from '../components/atoms/card_icons/CardIcons';
-import { useAuth } from '../contexts/Auth';
+import { NativeStackScreenProps } from '@react-navigation/native-stack';
+import React, { useCallback, useLayoutEffect, useMemo, useRef } from 'react';
+import { ScrollView, StyleSheet, View } from 'react-native';
+import { IconButton, Text } from 'react-native-paper';
+import Share from 'react-native-share';
 import Icon from 'react-native-vector-icons/MaterialCommunityIcons';
+import LogDetailBottomSheet from '../components/atoms/log_detail/LogDetailBottomSheet';
+import { useAuth } from '../contexts/Auth';
+import { AppStackParamList } from '../navigation/AppStack';
 
 type LogDetailProps = NativeStackScreenProps<AppStackParamList, 'LogDetail'>;
 
 const LogDetail = ({ route, navigation }: LogDetailProps) => {
 
-    const { id, userId, dateTimeOfActivity, notes, categories, mood} = route.params;
+    const { id, userId, dateTimeOfActivity, notes, categories, mood } = route.params;
 
     const { authData } = useAuth();
 
@@ -33,15 +32,38 @@ const LogDetail = ({ route, navigation }: LogDetailProps) => {
         console.log('handle sheet changes', index);
     }, []);
 
+    const handleSocialMediaShare = () => {
+        const categoryString = (categories && categories.length > 0) ? ("\n\nCategories: " + categories.join(', ')) : "";
+        const moodString = (mood && mood.length > 0) ? ("\n\nMood: " + mood.join(', ')) : "";
+        const noteString = notes ? ("\n\n" + notes) : "";
+
+        const message = `${date.toLocaleString([], {
+            weekday: 'long',
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit'
+        })}${categoryString}${moodString}${noteString}`;
+
+        Share.open({ message: message })
+            .then((res) => {
+                console.log(res);
+            })
+            .catch((err) => {
+                err && console.log(err);
+            });
+    };
+
     useLayoutEffect(() => {
         if (userId === authData?.id) {
             navigation.setOptions({
                 headerRight: () => (
-                    <IconButton 
+                    <IconButton
                         icon="dots-vertical"
                         size={20}
-                        onPress={handlePresentModalPress} 
-                        hasTVPreferredFocus={undefined} 
+                        onPress={handlePresentModalPress}
+                        hasTVPreferredFocus={undefined}
                         tvParallaxProperties={undefined}
                     />
                 ),
@@ -53,12 +75,12 @@ const LogDetail = ({ route, navigation }: LogDetailProps) => {
         {
             name: "Delete",
             onPress: () => {
-              navigation.navigate('LogDelete')
-              console.log('pressed!');
+                navigation.navigate('LogDelete');
+                console.log('pressed!');
             },
         },
         {
-            name: "Share", 
+            name: "Share to User",
             onPress: () => {
                 navigation.navigate('ShareScreen', {
                     sharedData: route.params,
@@ -67,29 +89,33 @@ const LogDetail = ({ route, navigation }: LogDetailProps) => {
             },
         },
         {
+            name: "Share to Social Media",
+            onPress: () => handleSocialMediaShare()
+        },
+        {
             name: "Label",
             onPress: () => {
-                navigation.navigate('AddLabel')
+                navigation.navigate('AddLabel');
                 console.log('pressed!');
             },
         },
         {
             name: "Edit",
             onPress: () => {
-                navigation.navigate('LogEdit')
+                navigation.navigate('LogEdit');
                 console.log('pressed!');
             },
         },
     ];
 
-    const activitiesToIcons = new Map()
-    activitiesToIcons.set('running', 'run') 
-    activitiesToIcons.set('yoga', 'yoga') 
-    activitiesToIcons.set('eating', 'food') 
-    activitiesToIcons.set('going out', 'party-popper') 
-    activitiesToIcons.set('spending time with friends', 'human-male-female')
-    activitiesToIcons.set('physical therapy', 'doctor')
-    activitiesToIcons.set('therapy', 'account-heart')
+    const activitiesToIcons = new Map();
+    activitiesToIcons.set('running', 'run');
+    activitiesToIcons.set('yoga', 'yoga');
+    activitiesToIcons.set('eating', 'food');
+    activitiesToIcons.set('going out', 'party-popper');
+    activitiesToIcons.set('spending time with friends', 'human-male-female');
+    activitiesToIcons.set('physical therapy', 'doctor');
+    activitiesToIcons.set('therapy', 'account-heart');
 
     return (
         <BottomSheetModalProvider>
@@ -98,23 +124,22 @@ const LogDetail = ({ route, navigation }: LogDetailProps) => {
                 <Text style={styles.time}>{date.toLocaleTimeString([], { hour12: true, hour: '2-digit', minute: '2-digit' })}</Text>
                 <View style={styles.categoriesContainer}>
                     {categories?.map(category => (
-                        <Text 
-                        style={styles.ActivityText}
-                        key={category}>
-                        <Icon key={category} name={activitiesToIcons.get(category)} size={20} />    {category} 
+                        <Text style={styles.ActivityText} key={category}>
+                            <Icon key={category} name={activitiesToIcons.get(category)} size={20} />
+                            {category}
                         </Text>
                     ))}
                 </View>
                 <View style={styles.mood}>
                     {mood?.map(category => (
-                        <Text 
-                        style={styles.MoodText}
-                        key={category}>Mood: {category}     </Text>
+                        <Text style={styles.MoodText} key={category}>
+                            Mood: {category}
+                        </Text>
                     ))}
                 </View>
                 <Text style={styles.NotesText}>Notes: </Text>
-                <Text style={styles.notes}>{notes} </Text>
-               
+                <Text style={styles.notes}>{notes}</Text>
+
             </ScrollView>
             <BottomSheetModal
                 ref={bottomSheetModalRef}
@@ -160,29 +185,28 @@ const styles = StyleSheet.create({
         fontSize: 20,
         paddingTop: 10,
     },
-    NotesText:{
+    NotesText: {
         fontSize: 15,
         paddingTop: 50,
         fontWeight: 'bold',
     },
-    MoodText:{
+    MoodText: {
         fontSize: 15,
         paddingTop: 15,
         fontWeight: 'bold',
         flex: 1,
     },
-    ActivityText:{
+    ActivityText: {
         fontSize: 15,
         fontWeight: 'bold',
         flex: 1,
         borderWidth: 3,
         borderColor: 'black',
-        padding : 2,
+        padding: 2,
         textAlign: 'center',
         margin: 4,
         backgroundColor: '#fff' //white
     },
-
 });
 
 export default LogDetail;
